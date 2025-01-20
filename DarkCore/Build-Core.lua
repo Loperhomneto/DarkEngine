06dfd58e15a6fe417b27e72387e889154d2517eb
Anshul <anshultheawesome@gmail.com>
2025-01-19 18:19:39 -0800

    fixed issue with spdlog

--- DarkCore/Build-Core.lua
+++ DarkCore/Build-Core.lua
@@ -9,7 +9,7 @@ project "DarkCore"
 
    includedirs
    {
-      "Source", "../Vendor/Glad/include", "../Vendor/glfw/include", "../Vendor/glm/glm", "../Vendor/stb", "../Vendor/Irrklang/include", "../Vendor/spdlog/include"
+      "Source", "../Vendor/Glad/include", "../Vendor/glfw/include", "../Vendor/glm/glm", "../Vendor/stb", "../Vendor/Irrklang/include", "../Vendor/spdlog/include", "../Vendor/imgui"
    }
 
    libdirs
@@ -19,7 +19,7 @@ project "DarkCore"
 
    links
    {
-       "GLFW", "opengl32.lib", "Glad", "irrKlang.lib", "SPDLOG"
+       "GLFW", "opengl32.lib", "Glad", "irrKlang.lib"
    }
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
