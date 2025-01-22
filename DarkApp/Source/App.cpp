0c0da76fba93511f4505aab078ce2154b7f8308f
Anshul <anshultheawesome@gmail.com>
2025-01-21 17:04:19 -0800

    reupdate todo

--- DarkApp/Source/App.cpp
+++ DarkApp/Source/App.cpp
@@ -11,7 +11,7 @@ void FooLayer::OnAttach()
 {
 	Logger::info("onattach app");
 
-//TODO: camera, imgui, framebuffers, unity like editor
+//TODO: camera, imgui, framebuffers, unity like editor, make so that renderer:init() or deinit() cant be called by app
 // add rotated quads
 // how to make a smokey or glowey texture / watch made game in hazel in an hour to see what else to add
 // spritesheets/how to render them
