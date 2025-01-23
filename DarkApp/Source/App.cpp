fcd22c87c7fe21e3fa13ee5945db6ba6392eb750
Anshul <anshultheawesome@gmail.com>
2025-01-22 21:23:27 -0800

    update todo

--- DarkApp/Source/App.cpp
+++ DarkApp/Source/App.cpp
@@ -12,7 +12,6 @@ void FooLayer::OnAttach()
 	Logger::info("onattach app");
 
 //TODO: camera, imgui, framebuffers, unity like editor, make so that renderer:init() or deinit() cant be called by app
-// fix logger not working with integers, logger::trace not working
 // add rotated quads
 // how to make a smokey or glowey texture / watch made game in hazel in an hour to see what else to add
 // spritesheets/how to render them
