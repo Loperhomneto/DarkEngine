976dd52eca2f2e0888730c35a089f3b5b772ac94
Anshul <anshultheawesome@gmail.com>
2025-01-22 21:23:17 -0800

    Fixed issue where logger wasn't working with integers and floats

--- DarkCore/Source/Dark/Log.cpp
+++ DarkCore/Source/Dark/Log.cpp
@@ -5,29 +5,36 @@
 namespace Dark
 {
 
-	void Log::Trace(const char* string)
-	{
-		spdlog::trace(string);
-	}
+#define CreateLoggerFunctions(Name, name) void Log::##Name(const char* string) { spdlog::##name(string); }\
+										  void Log::##Name(int integer) { spdlog::##name(integer); }\
+										  void Log::##Name(bool boolean) { spdlog::##name(boolean); }\
+										  void Log::##Name(float floating) { spdlog::##name(floating); }\
+										  void Log::##Name(double doubling) { spdlog::##name(doubling); }
+									
+	CreateLoggerFunctions(Trace, trace);
+	CreateLoggerFunctions(Info, info);
+	CreateLoggerFunctions(Warn, warn);
+	CreateLoggerFunctions(Error, error);
+	CreateLoggerFunctions(Critical, critical);
 
-	void Log::Info(const char* string)
-	{
-		spdlog::info(string);
-	}
+	//void Log::Info(const char* string)
+	//{
+	//	spdlog::info(string);
+	//}
 
-	void Log::Warn(const char* string)
-	{
-		spdlog::warn(string);
-	}
+	//void Log::Warn(const char* string)
+	//{
+	//	spdlog::warn(string);
+	//}
 
-	void Log::Error(const char* string)
-	{
-		spdlog::error(string);
-	}
+	//void Log::Error(const char* string)
+	//{
+	//	spdlog::error(string);
+	//}
 
-	void Log::Critical(const char* string)
-	{
-		spdlog::critical(string);
-	}
+	//void Log::Critical(const char* string)
+	//{
+	//	spdlog::critical(string);
+	//}
 
 }
--- DarkCore/Source/Dark/Log.h
+++ DarkCore/Source/Dark/Log.h
@@ -3,14 +3,24 @@
 namespace Dark
 {
 
+#define CreateLoggerHeaders(Name) void static Name(const char* string);\
+								  void static Name(int integer);\
+								  void static Name(float floating);\
+								  void static Name(double doubling);\
+								  void static Name(bool boolean);
 	class Log
 	{
 	public:
-		void static Trace(const char* string);
-		void static Info(const char* string);
-		void static Warn(const char* string);
-		void static Error(const char* string);
-		void static Critical(const char* string);
+		//void static Trace(const char* string);
+		//void static Info(const char* string);
+		//void static Warn(const char* string);
+		//void static Error(const char* string);
+		//void static Critical(const char* string);
+		CreateLoggerHeaders(Trace)
+		CreateLoggerHeaders(Info)
+		CreateLoggerHeaders(Warn)
+		CreateLoggerHeaders(Error)
+		CreateLoggerHeaders(Critical)
 	};
 
 }
